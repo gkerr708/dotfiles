abdb16493580a88a8ad94354e0aa64bad462318a
gkerr_laptop <gavin.kerr@dal.ca>
2026-05-21 14:06:49 -0300

    Fixed markdown preview

--- home/.config/nvim/lua/plugins/markdown.lua
+++ home/.config/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,20 @@
 return {
-  {
+-- install without yarn or npm
+{
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-    init = function()
-      vim.g.mkdp_browser = 'firefox'
-    end,
-  }
+}
+
+---- install with yarn or npm
+--{
+--  "iamcco/markdown-preview.nvim",
+--  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--  build = "cd app && yarn install",
+--  init = function()
+--    vim.g.mkdp_filetypes = { "markdown" }
+--  end,
+--  ft = { "markdown" },
+--},
 }
